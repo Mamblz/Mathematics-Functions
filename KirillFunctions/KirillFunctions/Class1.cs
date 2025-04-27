2efcea390989ac8678da9eba4bc44bd934883c27
kobza <kobza0406@gmail.com>
2025-04-27 15:41:13 +0300

    Реализация метода для вычисления десятичного логарифма

--- KirillFunctions/KirillFunctions/Class1.cs
+++ KirillFunctions/KirillFunctions/Class1.cs
@@ -8,6 +8,14 @@ namespace KirillFunctions
 {
     public class Class1
     {
+        public static double Logarithm10(double a)
+        {
+            if (a <= 0)
+            {
+                throw new ArgumentException("Десятичный логарифм не существует для отрицательных чисел или нуля.");
+            }
 
+            return Math.Log10(a);
+        }
     }
 }
