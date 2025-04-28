363e1dc628762669010a25192a6295eaa679af8e
Вершининап <@105-01>
2025-04-28 11:13:56 +0300

    cod

---
+++ KirillFunctions/Ploshadi/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploshadi
+{
+    public class Class1
+    {
+        // Метод для вычисления площади квадрата
+        public static double CalculateSquareArea(double sideLength)
+        {
+            if (sideLength <= 0)
+            {
+                throw new ArgumentException("Длина стороны квадрата должна быть положительным числом");
+            }
+            return sideLength * sideLength;
+        }
+
+        // Метод для вычисления радиуса круга по его площади
+        public static double CalculateCircleRadius(double area)
+        {
+            if (area <= 0)
+            {
+                throw new ArgumentException("Площадь круга должна быть положительным числом");
+            }
+            return Math.Sqrt(area / Math.PI);
+        }
+
+        // Метод для вычисления периметра прямоугольника
+        public static double CalculateRectanglePerimeter(double length, double width)
+        {
+            if (length <= 0 || width <= 0)
+            {
+                throw new ArgumentException("Длины сторон прямоугольника должны быть положительными числами");
+            }
+            return 2 * (length + width);
+        }
+    }
+}
---
+++ KirillFunctions/Ploshadi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Ploshadi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ploshadi")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("5af35ad5-e477-4d09-93bc-3035c0778490")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
